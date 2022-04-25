8a157f80daef42a1d8b45b4bbc31aabc6c9ce636
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-26 02:48:21 +0600

    used jwt token also for security purpose in order history done some try catch response error handle

--- src/Pages/OrderHistory/OrderHistory.js
+++ src/Pages/OrderHistory/OrderHistory.js
@@ -1,28 +1,49 @@
 import { async } from "@firebase/util";
 import axios from "axios";
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
+import Loading from "../Shared/Loading/Loading";
 
 const OrderHistory = () => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const [order, setOrder] = useState([]);
   console.log(user);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getOrders = async () => {
-      await axios
-        .get(`http://localhost:5000/orderhistory?email=${user?.email}`)
-        .then((res) => setOrder(res?.data));
+      try {
+        await axios
+          .get(`http://localhost:5000/orderhistory?email=${user?.email}`, {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+          })
+          .then((res) => setOrder(res?.data));
+      } catch (error) {
+        console.log(error.response.status);
+        if (error.response.status === 401 || error.response.status === 403) {
+          signOut(auth);
+          navigate("/login");
+        }
+      }
     };
     getOrders();
   }, [user]);
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <div>
       <h1>Your Order History here</h1>
 
       {order.map((data) => (
-        <p>{data?.service}</p>
+        <p key={data._id}>{data?.service}</p>
       ))}
     </div>
   );
